f5a1d1c60cfc403bf3fce700f98998089d162729
AdamZkingspleasure <adam.zadorski@kingspleasure.com>
2022-12-02 19:58:38 +0100

    Fixed player movement

--- Assets/Stock/Scripts/PlayerRelated/InputController.cs
+++ Assets/Stock/Scripts/PlayerRelated/InputController.cs
@@ -6,7 +6,12 @@ public class InputController : MonoBehaviour
 {
     public PlayerProporties playerProporties;
     private Rigidbody2D rb;
+    private CapsuleCollider2D capsuleCollider;
     private Block lastMinedBlock;
+
+    public Vector2 inputValue;
+    public Vector2 moveVector;
+    public LayerMask groundLayer;
 
     private void Awake()
     {
@@ -14,30 +19,97 @@ public class InputController : MonoBehaviour
     }
     private void IntializePlayer()
     {
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
         MouseInput();
-        PlayerMovement();
+        KeyboardInput();
+
+        rb.velocity = moveVector;
+    }
+    private void LateUpdate()
+    {
+        HorizontalAirboneMovement();
+        VerticalAirboneMovement();
+        HorizontalGroundedMovement();
     }
 
-    private void PlayerMovement()
+    private void HorizontalGroundedMovement()
+    {
+        if (IsGrounded())
+        {
+            if(inputValue!=Vector2.zero)
+            {
+                moveVector = Vector2.Lerp(moveVector, new Vector2(inputValue.x * playerProporties.maxGroundedSpeed, moveVector.y), Time.deltaTime * playerProporties.horizontalGroundedAcceleration);
+            }
+            else
+            {
+                moveVector = Vector2.Lerp(moveVector, new Vector2(inputValue.x * playerProporties.maxGroundedSpeed, moveVector.y), Time.deltaTime * playerProporties.groundedSpeedLose);
+            }
+           
+        }
+    }
+
+
+    private void HorizontalAirboneMovement()
+    {
+        if (!IsGrounded())
+        {
+            moveVector = Vector2.Lerp(moveVector, new Vector2(inputValue.x * playerProporties.maxHorizontalAirboneSpeed, moveVector.y), Time.deltaTime * playerProporties.horizontalAirboneSpeedLose);
+        }
+    }
+
+    private void VerticalAirboneMovement()
+    {
+        if (!IsGrounded())
+        {
+            moveVector = Vector2.Lerp(moveVector, new Vector2(moveVector.x, moveVector.y - playerProporties.gravity), Time.deltaTime * playerProporties.verticalAirboneSpeedLose);
+        }
+        else
+        {
+            moveVector.y = 0;
+        }
+    }
+
+
+    private bool IsGrounded()
+    {
+        Vector2 position = capsuleCollider.transform.position + (Vector3)capsuleCollider.offset - new Vector3(capsuleCollider.size.x/2, capsuleCollider.size.y / 2) - new Vector3(0,0.1f,0);
+        Vector2 direction = Vector2.right;
+        float distance = capsuleCollider.size.x / 2;
+
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+
+
+    private void Jump()
+    {
+        if (!IsGrounded()) return;
+        transform.position += new Vector3(0, 0.1f, 0);
+        moveVector = new Vector2(moveVector.x, playerProporties.jumpForce);
+    }
+    private void KeyboardInput()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
-        gameObject.transform.position = new Vector2(transform.position.x + (h * playerProporties.movementSpeed),
-           transform.position.y + (v * playerProporties.movementSpeed));
+        inputValue = new Vector2(h, v);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2(0, playerProporties.jumpForce);
+            Jump();
         }
-
     }
-
     private void MouseInput()
     {
         if (Input.GetMouseButton(0))
@@ -48,10 +120,10 @@ public class InputController : MonoBehaviour
             {
                 if (hit.collider.GetComponent<Block>())
                 {
-                   
-                    if(lastMinedBlock!=null)
+
+                    if (lastMinedBlock != null)
                     {
-                        if(lastMinedBlock != hit.collider.GetComponent<Block>())
+                        if (lastMinedBlock != hit.collider.GetComponent<Block>())
                         {
                             lastMinedBlock.OnStopMining();
                         }
@@ -65,11 +137,11 @@ public class InputController : MonoBehaviour
         }
         else
         {
-            if(lastMinedBlock!=null)
+            if (lastMinedBlock != null)
             {
                 lastMinedBlock.OnStopMining();
             }
-            
+
         }
     }
 }
--- Assets/Stock/Scripts/PlayerRelated/PlayerProporties.cs
+++ Assets/Stock/Scripts/PlayerRelated/PlayerProporties.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerProporties", menuName = "ScriptableObjects/PlayerProporties", order = 1)]
 public class PlayerProporties : ScriptableObject
 {
+    public float verticalAirboneSpeedLose;
+    public float horizontalAirboneSpeedLose;
+
+    public float horizontalGroundedAcceleration;
+
+
+    public float maxHorizontalAirboneSpeed;
+
+    public float groundedSpeedLose;
+    public float maxGroundedSpeed;
     public float movementSpeed;
     public float jumpForce;
+    public float gravity;
 }
