f3bfa5e6b3a1172425a7c0b37ce292e58cdc8331
Michal <none@none.com>
2022-12-10 16:33:58 +0100

    Ground

--- Assets/Stock/Scripts/WorldRelated/WorldGenerator.cs
+++ Assets/Stock/Scripts/WorldRelated/WorldGenerator.cs
@@ -7,8 +7,7 @@ using UnityEngine.Tilemaps;
 
 public class WorldGenerator : MonoBehaviour
 {
-    int worldWidth = 100;
-    int worldHeight = 100;
+    const int mapConst = 8;
 
     [SerializeField] private WorldParameters worldParameters;
     [SerializeField] private Tilemap tileMap;
@@ -16,27 +15,61 @@ public class WorldGenerator : MonoBehaviour
 
     private void Start()
     {
-        GenerateWorld();
+        GenerateWorld(MapSize.small);
     }
 
-    private void GenerateWorld()
+    private void GenerateWorld(MapSize mapSize)
     {
-        int wl = - worldWidth / 2;
-        int wr = worldWidth / 2;
+        GetMapSize(mapSize, out int width, out int height);
         
-        int hu = 0;
-        int hd = -worldHeight / 2;
+        int x0 = - mapConst * width / 2;
+        int x1 = - x0;
 
-        for (int x = wl; x < wr; x++)
+        int y0 = 0;
+        int y1 = - mapConst * height / 2 - mapConst / 2;
+
+        for (int x = x0; x < x1; x++)
         {
-            for (int y = hu; hd < y; y--)
+            for (int y = y0; y1 < y; y--)
             {
                 var tilePos = tileMap.WorldToCell(new Vector2(x, y));
                 tileMap.SetTile(tilePos, tile);
             }
         }
     }
+
+    private void GetMapSize(MapSize mapSize, out int width, out int height)
+    {
+        switch (mapSize) {
+            case MapSize.small:
+                {
+                    width = mapConst * 6;
+                    height = mapConst * 2;
+                    break;
+                }
+            case MapSize.medium:
+                {
+                    width = mapConst * 10;
+                    height = mapConst * 3;
+                    break;
+                }
+            case MapSize.large:
+                {
+                    width = mapConst * 14;
+                    height = mapConst * 4;
+                    break;
+                }
+            default:
+                {
+                    throw new Exception("Error");
+                }
+        }
+    }
+
+    enum MapSize
+    {
+        small = 0,
+        medium = 1,
+        large = 2
+    }
 }
-
-
-
