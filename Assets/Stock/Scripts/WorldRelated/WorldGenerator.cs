af841a45236714492fe0c1dee8594b717b4a1545
Michal <none@none.com>
2022-12-10 15:35:20 +0100

    Ground

--- Assets/Stock/Scripts/WorldRelated/WorldGenerator.cs
+++ Assets/Stock/Scripts/WorldRelated/WorldGenerator.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class WorldGenerator : MonoBehaviour
 {
+    int worldWidth = 100;
+    int worldHeight = 100;
 
     [SerializeField] private WorldParameters worldParameters;
     [SerializeField] private Tilemap tileMap;
@@ -18,11 +21,17 @@ public class WorldGenerator : MonoBehaviour
 
     private void GenerateWorld()
     {
-        for (int x = 0; x <= worldParameters.worldSize.x; x++)
+        int wl = - worldWidth / 2;
+        int wr = worldWidth / 2;
+        
+        int hu = 0;
+        int hd = -worldHeight / 2;
+
+        for (int x = wl; x < wr; x++)
         {
-            for (int y = 0; y <= worldParameters.worldSize.y; y++)
+            for (int y = hu; hd < y; y--)
             {
-                var tilePos = tileMap.WorldToCell(new Vector2(x,y));
+                var tilePos = tileMap.WorldToCell(new Vector2(x, y));
                 tileMap.SetTile(tilePos, tile);
             }
         }
