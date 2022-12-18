0847f0c62de0a6d4166a3ec49f94f949c51abebb
Michal <none@none.com>
2022-12-18 20:36:16 +0100

    ground

--- Assets/Stock/Scripts/WorldRelated/WorldGenerator.cs
+++ Assets/Stock/Scripts/WorldRelated/WorldGenerator.cs
@@ -17,7 +17,8 @@ public class WorldGenerator : MonoBehaviour
 
     private void Start()
     {
-        GenerateWorld(MapSize.small);
+        //GenerateWorld(MapSize.small);
+        GenerateWorldV2();
     }
 
     private void GenerateWorld(MapSize mapSize)
@@ -36,6 +37,59 @@ public class WorldGenerator : MonoBehaviour
         {
             yt += random.Next(-1, 2); ;
             for (int y = yt; y1 < y; y--)
+            {
+                var tilePos = tileMap.WorldToCell(new Vector2(x, y));
+                tileMap.SetTile(tilePos, tile);
+            }
+        }
+    }
+
+
+    private void GenerateWorldV2()
+    {
+        int k = 2;
+
+        int tr = 10;
+        int size_x = 200;
+        int size_y = - 20;
+
+        int max_y = 15;
+        int min_y = - 10;
+        var listY = new List<int>();
+
+        int x = 0;
+        int y = 0;
+        var rand = new System.Random();
+        for (x = 0; x < size_x + tr; x++)
+        {
+            y += rand.Next(-1 - k, 2 + k);
+
+            if (max_y < y)
+            {
+                y = max_y;
+            }
+            else if (y < min_y)
+            {
+                y = min_y;
+            }
+
+            listY.Add(y);
+        }
+
+        int y2;
+        var listY2 = new List<int>();
+        for (int i0 = tr; i0 < size_x + tr; i0++)
+        {
+            y2 = 0;
+            for (int i1 = 0; i1 < tr; i1++)
+                y2 += listY[i0 - i1];
+
+            listY2.Add(y2 / tr);
+        }
+
+        for (x = 0; x < size_x; x++)
+        {
+            for (y = listY2[x]; y > size_y; y--)
             {
                 var tilePos = tileMap.WorldToCell(new Vector2(x, y));
                 tileMap.SetTile(tilePos, tile);
